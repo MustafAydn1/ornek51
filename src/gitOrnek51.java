449b464f04ef42ba3f1e6ed1fcacdf029142eb91
Mustafa Aydin <mustafakilic3520@gmail.com>
2020-11-24 23:34:04 +0300

    Initial commit

---
+++ src/gitOrnek51.java
@@ -0,0 +1,8 @@
+public class gitOrnek51 {
+    public static void main(String[] args) {
+        System.out.println("ornek51 ");
+        System.out.println("mustafa Aydın gidecek artık");
+
+        System.out.println("bunu sonra silecem. görecem. ne oluyor");
+    }
+}
